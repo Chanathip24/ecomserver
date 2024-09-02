b0342e058b6c48afecf70bae808450e532004cc9
Chanathip <chanathip.cha@outlook.com>
2024-09-02 20:02:57 +0700

    fix safari

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ app.use(
 app.use(cookieparser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const checkauthadmin = require("./services/middleware")
+const checkauthadmin = require("./services/middleware");
 
 //create cookie
-const setcookie = (res,key,value)=>{
+const setcookie = (res, key, value) => {
   res.cookie(key, value, {
     maxAge: 1000 * 60 * 60 * 24,
     httpOnly: true,
-    secure: process.env.NODE_ENV ==='production',
-    sameSite : "None"
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "None",
+    domain: "ecomserver-xgr1.onrender.com",
   });
-
-}
+};
 
 app.post("/register", (req, res) => {
   //check validatorresult
@@ -43,11 +43,11 @@ app.post("/register", (req, res) => {
     mysql.query(query, [data], (err, value) => {
       if (err) return res.status(500).json(err);
       const token = jwt.sign(
-        { email: req.body.email , role : "MEMBER" },
+        { email: req.body.email, role: "MEMBER" },
         process.env.JWT_SECRET,
         { expiresIn: "1d" }
       );
-      setcookie(res,"token",token)
+      setcookie(res, "token", token);
 
       res.status(200).json({ message: "Insert success", value });
       res.end();
@@ -56,26 +56,27 @@ app.post("/register", (req, res) => {
 });
 
 //get all users data
-app.get('/users',checkauthadmin,(req,res)=>{
-  const query = "SELECT id,email,fname,lname,created_at,role FROM users"
-  mysql.query(query,(err,result)=>{
-    if(err) return res.status(500).json({err})
-    if(result.length === 0 ) return res.status(404).json({msg : "no data kub pom"})
-    res.status(200).json({result : result})
+app.get("/users", checkauthadmin, (req, res) => {
+  const query = "SELECT id,email,fname,lname,created_at,role FROM users";
+  mysql.query(query, (err, result) => {
+    if (err) return res.status(500).json({ err });
+    if (result.length === 0)
+      return res.status(404).json({ msg: "no data kub pom" });
+    res.status(200).json({ result: result });
     res.end();
-  })
-})
+  });
+});
 
 //delete user
-app.delete('/users/delete/:id',checkauthadmin,(req,res)=>{
-  const query = "DELETE FROM users where id = ?"
-  const id = req.params.id
-  mysql.query(query,[id],(err)=>{
-    if(err) res.json({msg : err})
-    res.status(200).json({msg : "Delete Success"})
-    res.end()
-  })
-})
+app.delete("/users/delete/:id", checkauthadmin, (req, res) => {
+  const query = "DELETE FROM users where id = ?";
+  const id = req.params.id;
+  mysql.query(query, [id], (err) => {
+    if (err) res.json({ msg: err });
+    res.status(200).json({ msg: "Delete Success" });
+    res.end();
+  });
+});
 
 //login
 app.post("/login", (req, res) => {
@@ -87,20 +88,20 @@ app.post("/login", (req, res) => {
 
     if (result.length === 0)
       return res.status(404).json({ msg: "No user found." });
-    
+
     const user = result[0];
     bcrypt.compare(req.body.password, user.password, (err, result) => {
-      if(err) res.status(500).json({msg : "Hash error"})
+      if (err) res.status(500).json({ msg: "Hash error" });
       if (result === false)
         return res.status(403).json({ msg: "password is wrong." });
 
       const token = jwt.sign(
-        { email: req.body.email,role: user.role },
+        { email: req.body.email, role: user.role },
         process.env.JWT_SECRET,
         { expiresIn: "1d" }
       );
       //create cookie
-      setcookie(res,"token",token)
+      setcookie(res, "token", token);
       res.status(200).json({ msg: "pass" });
       res.end();
     });
@@ -108,23 +109,28 @@ app.post("/login", (req, res) => {
 });
 
 //logout
-app.get('/logout', (req, res) => {
-  const token = req.cookies['token'];
+app.get("/logout", (req, res) => {
+  const token = req.cookies["token"];
   if (!token) {
     return res.json({ msg: token });
   }
-  res.clearCookie('token',{sameSite:"None",secure: process.env.NODE_ENV ==='production',path: '/'});
+  res.clearCookie("token", {
+    sameSite: "None",
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    domain: "ecomserver-xgr1.onrender.com",
+  }); //for deploy ,
   res.json({ msg: "pass" });
 });
 
 //checkcookie
 app.get("/checkcookie", (req, res) => {
   const cookie = req.cookies["token"];
-  if (!cookie) return res.json({msg: "no cookie"});
+  if (!cookie) return res.json({ msg: "no cookie" });
   jwt.verify(cookie, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.json({msg : "no cookie"});
-    const role = decoded.role
-    res.json({msg : "pass" , role});
+    if (err) return res.json({ msg: "no cookie" });
+    const role = decoded.role;
+    res.json({ msg: "pass", role });
   });
 
   res.end();
@@ -132,5 +138,5 @@ app.get("/checkcookie", (req, res) => {
 app.listen(process.env.PORT, (err) => {
   if (err) return err;
   console.log(`connected to server on port ${process.env.PORT}`);
-  console.log(process.env.NODE_ENV)
+  console.log(process.env.NODE_ENV);
 });
