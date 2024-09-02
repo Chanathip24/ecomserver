18e2e7ca462db2a3bacdf846e281b1348a4cf03e
Chanathip <chanathip.cha@outlook.com>
2024-09-02 18:43:22 +0700

    Some api

--- index.js
+++ index.js
@@ -9,17 +9,27 @@ const app = express();
 require("dotenv").config();
 //middleware
 app.use(
-  cors(
-    {
-      origin: [process.env.URL],
-      credentials: true,
-    },
-  )
+  cors({
+    origin: [process.env.URL],
+    credentials: true,
+  })
 );
 app.use(cookieparser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const registervalidator = require("./services/middleware");
+const checkauthadmin = require("./services/middleware")
+
+//create cookie
+const setcookie = (res,key,value)=>{
+  res.cookie(key, value, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+    secure: process.env.NODE_ENV ==='production',
+    sameSite : "none"
+  });
+
+}
 
 app.post("/register", registervalidator, (req, res) => {
   //check validatorresult
@@ -38,59 +48,90 @@ app.post("/register", registervalidator, (req, res) => {
         process.env.JWT_SECRET,
         { expiresIn: "1d" }
       );
+      setcookie(res,"token",token)
 
-      res.cookie("token", token, {
-        maxAge: 1000 * 60 * 60 * 24,
-        httpOnly: true,
-        secure: false,
-      });
       res.status(200).json({ message: "Insert success", value });
       res.end();
     });
   });
 });
 
+//get all users data
+app.get('/users',checkauthadmin,(req,res)=>{
+  const query = "SELECT id,email,fname,lname,created_at,role FROM users"
+  mysql.query(query,(err,result)=>{
+    if(err) return res.status(500).json({err})
+    if(result.length === 0 ) return res.status(404).json({msg : "no data kub pom"})
+    res.status(200).json({result : result})
+    res.end();
+  })
+})
+
+//delete user
+app.delete('/users/delete/:id',checkauthadmin,(req,res)=>{
+  const query = "DELETE FROM users where id = ?"
+  const id = req.params.id
+  mysql.query(query,[id],(err)=>{
+    if(err) res.json({msg : err})
+    res.status(200).json({msg : "Delete Success"})
+    res.end()
+  })
+})
+
+//login
 app.post("/login", (req, res) => {
   const query = "SELECT * FROM users where email=  ?";
+
+  //start query
   mysql.query(query, [req.body.email], (err, result) => {
     if (err) return res.status(500).json({ msg: err });
 
     if (result.length === 0)
       return res.status(404).json({ msg: "No user found." });
-
+    
     const user = result[0];
     bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if(err) res.status(500).json({msg : "Hash error"})
       if (result === false)
         return res.status(403).json({ msg: "password is wrong." });
 
       const token = jwt.sign(
-        { email: req.body.email },
+        { email: req.body.email,role: user.role },
         process.env.JWT_SECRET,
         { expiresIn: "1d" }
       );
-      res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        maxAge: 1000 * 60 * 60 * 24,
-        sameSite: 'None'
-      });
+      //create cookie
+      setcookie(res,"token",token)
       res.status(200).json({ msg: "pass" });
       res.end();
     });
   });
 });
 
+//logout
+app.get('/logout', (req, res) => {
+  const token = req.cookies['token'];
+  if (!token) {
+    return res.json({ msg: token });
+  }
+  res.clearCookie('token');
+  res.json({ msg: "pass" });
+});
+
+//checkcookie
 app.get("/checkcookie", (req, res) => {
   const cookie = req.cookies["token"];
-  if (!cookie) return res.json("no cookie");
+  if (!cookie) return res.json({msg: "no cookie"});
   jwt.verify(cookie, process.env.JWT_SECRET, (err, decoded) => {
-    if(err) return res.json("Cookie is wrong")
-    res.json(decoded)
+    if (err) return res.json({msg : "no cookie"});
+    const role = decoded.role
+    res.json({msg : "pass" , role});
   });
-  
+
   res.end();
 });
 app.listen(process.env.PORT, (err) => {
   if (err) return err;
   console.log(`connected to server on port ${process.env.PORT}`);
+
 });
--- services/middleware.js
+++ services/middleware.js
@@ -1,7 +1,22 @@
-const {body} = require('express-validator')
+const {body, check} = require('express-validator')
+const jwt = require('jsonwebtoken')
 
 const registervalidator = [
     body('email').isLength({min:1}).isEmail().trim().escape().withMessage("Email error")
 ]
 
-module.exports = registervalidator;
+const checkauthadmin = (req, res, next) => {
+    
+    const token = req.cookies['token'];
+    if(!token) return res.status(401).json({msg: "No token "});
+
+    jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
+        if(err) return res.status(401).json({msg : "Unauthorized token error"})
+        if(decoded.role !== "ADMIN") return res.status(401).json({msg : "You are not an admin"})
+        req.user = decoded
+        next()
+    })
+
+};
+
+module.exports = [checkauthadmin,registervalidator];
