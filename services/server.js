8282d288d2226c4fd0c4b26f2bf04eb9a6bd4582
Chanathip <chanathip.cha@outlook.com>
2024-09-05 18:17:08 +0700

    pool

--- services/server.js
+++ services/server.js
@@ -14,32 +14,19 @@ var db_config = {
   enableKeepAlive: true, // Keeps the connection alive
   connectTimeout: 10000,
 }
-function connectToDatabase() {
-  connection = mysql.createConnection(db_config);
-
-  connection.connect((err) => {
-    if (err) {
-      
-      console.error('Error connecting to MySQL:', err);
-      setTimeout(connectToDatabase, 2000); // Retry after 2 seconds
-    } else {
-      console.log('Connected to MySQL cloud database.');
-    }
-  });
-
-  // Handle connection errors and reconnection
-  connection.on('error', (err) => {
-    console.error('MySQL Error:', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ETIMEDOUT' || err.code === 'ECONNRESET') {
-      console.error('MySQL connection lost, reconnecting...');
-      connectToDatabase();  // Reconnect on connection loss
-    } else {
-      throw err;
-    }
-  });
-}
-
-// Initialize connection
-connectToDatabase();
+connection = mysql.createPool({
+  host: process.env.db_url,
+  port : process.env.DB_PORT,
+  user : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_NAME,
+  ssl:{
+    rejectUnauthorized : true
+  },
+  connectTimeout : 10000
+})
+connection.on('error',(err)=>{
+  console.log(err)
+})
 
 module.exports = connection;
